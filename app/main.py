2e0f0070e0571d1b5d2c6ca8f9a0b3aeeba255f7
John Fizer <fizerco@gmail.com>
2026-01-07 03:09:34 -0600

    Fix syntax error in main.py

--- app/main.py
+++ app/main.py
@@ -27,6 +27,7 @@ import os
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT",8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", , reload = True)
+    uvicorn.run("app.main:app", host="0.0.0.0", reload = True)
+
 
 
